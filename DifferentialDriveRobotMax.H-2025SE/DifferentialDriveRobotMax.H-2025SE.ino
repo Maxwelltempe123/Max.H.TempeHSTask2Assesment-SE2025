759e8a7662a7bbed6439a6bd91e2c9041feb1a95
Maxwelltempe123 <maxwell.hrycykoh@education.nsw.gov.au>
2024-08-01 09:16:19 +1000

    too

---
+++ DifferentialDriveRobotMax.H-2025SE/DifferentialDriveRobotMax.H-2025SE.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+class Servo
+{
+
+}
+
+Servo servo9;
+Servo servo10;
+
+void setup()
+{
+  servo9.attach(9, 700, 2300);
+  servo10.attach(10, 700, 2300);
+}
+
+void loop()
+{
+  servo9.writeMicroseconds(1500);
+  servo10.writeMicroseconds(1500);
+  delay(100)
+}
+
+
+jkfdskfds
