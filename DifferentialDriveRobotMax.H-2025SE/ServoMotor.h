07f3be761aa5c3afd1d3e530b2c76f38fe0f58b8
Maxwelltempe123 <maxwell.hrycykoh@education.nsw.gov.au>
2024-08-06 20:48:37 +1000

    tester

---
+++ DifferentialDriveRobotMax.H-2025SE/ServoMotor.h
@@ -0,0 +1,18 @@
+#ifndef SERVOMOTOR_H
+#define SERVOMOTOR_H
+
+#include <Servo.h>
+
+class ServoMotor {
+  private:
+    Servo motor;
+    int pin;
+  
+  public:
+    ServoMotor(int pin);
+    void attach();
+    void setSpeed(int speed);
+    void stop();
+};
+
+#endif
