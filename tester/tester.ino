1fc811968792063436fd2ae611ed4aede68de922
Maxwelltempe123 <maxwell.hrycykoh@education.nsw.gov.au>
2024-08-09 09:03:00 +1000

    Update tester.ino

--- tester/tester.ino
+++ tester/tester.ino
@@ -1,7 +1,7 @@
 #include "ServoMotor.h"
 
-#define LEFT_SERVO_PIN 9
-#define RIGHT_SERVO_PIN 10
+#define LEFT_SERVO_PIN 10
+#define RIGHT_SERVO_PIN 9
 
 ServoMotor leftServo(LEFT_SERVO_PIN);
 ServoMotor rightServo(RIGHT_SERVO_PIN);
